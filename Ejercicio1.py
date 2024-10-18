efc658965f5d2719f8215c3cf759d35e80617624
sgimenelan <sgimenelan@educacion.navarra.es>
2024-10-18 08:35:54 +0200

    Update Ejercicio1.py

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,5 +1,3 @@
-nombre = input("Dime tu nombre ")
+nombre = str(input("Dime tu nombre "))
 numero = int(input("Dime el numero de veces que quieres que se repita "))
-while numero > 0:
-    print(nombre)
-    numero = (numero - 1)
+print((nombre + "\n") * numero)
